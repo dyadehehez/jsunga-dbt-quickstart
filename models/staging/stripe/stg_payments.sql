c501fcf1fef8bee9e1d2a545dba5c26a2bc89dbb
Jade Adrian Sunga <jsunga@stratpoint.com>
2023-10-17 14:31:32 +0000

    added everything

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
+    {{cents_to_dollars('amount',3)}}, --should be enclosed in a qoutation
     created as created_at
 from {{ source('stripe', 'payment') }}
